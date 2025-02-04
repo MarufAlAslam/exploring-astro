e81103e8c21b002b3fa5f5c45432ea65489a1b73
marufthenoob <maruf.taskcodigital@gmail.com>
2025-02-04 12:47:21 +0600

    static page added

--- src/components/react-components/hello.jsx
+++ src/components/react-components/hello.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 
 const Hello = ({ name, age, location }) => {
+  const [text, setText] = React.useState(
+    `Hello!! I am ${name}, I am ${age} years old, and I'm from ${location}`
+  );
   return (
     <div>
-      Hello!! I am {name}, I am {age} years old, and I'm from {location}
+      <h2 className="text-6xl font-bold text-center text-white">{text}</h2>
+
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => {
+          setText("Hello!! I am a React Component");
+          console.log("Button clicked");
+        }}
+      >
+        Click me
+      </button>
     </div>
   );
 };
